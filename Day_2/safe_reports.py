24be394874ae6329706f02db65def8f667c1da0f
cameronotoole44 <cameronotoole404@gmail.com>
2024-12-02 21:59:34 -0500

    better break down of what to do

---
+++ Day_2/safe_reports.py
@@ -0,0 +1,3 @@
+# the report is safe if the levels are strictly increasing or decreasing, any two adjacent levels differ by at least one and at most 3. create the function that checks if report is safe
+
+# 
